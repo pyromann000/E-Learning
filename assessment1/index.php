02d1b7eab2d8dce6f46b53fcc3430bab8941c6f4
pyromann000 <sangriajoaquin12l@gmail.com>
2024-09-27 15:34:10 +0800

    Add files via upload

---
+++ assessment1/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="quiz-container">
+    <h1>Multiple Choice Quiz</h1>
+    <hr>
+    <form id="quiz-form">
+        <div class="d-flex">
+            <div class="question">
+                <p> 1. Complete the sentence: <br>
+
+The _________ is a line of code where it is used to create __________, declare data and its data types, and other ____________. </p>
+<div class="answers">
+    <label>
+        <input type="radio" name="q1" value="a">
+        Objects
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        Class
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        Method
+    </label>
+</div>
+        <div class="question">
+                <p> 2. Complete the sentence: <br>
+
+                public _____   Lesson 3{ <br>
+	public static void ____ (String [] args){ <br>
+
+	System.out.______(“Hello World!”); <br>
+____ <br>
+}
+
+<div class="answers">
+    <label>
+        <input type="radio" name="q1" value="a">
+        }
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        main
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        class
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        println
+    </label>
+</div>
+    <div class="question">
+                <p> 3. Declare an integer variable:<br>
+
+                Choose the correct declaration: ___________ <br>
+
+              A.  int 87; <br>
+              B.  int b = ‘23’;<br>
+              C.  int c = 2.12;<br>
+              D.  int d = 2986499;<br>
+
+<div class="answers">
+    <label>
+        <input type="radio" name="q1" value="a">
+        A
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        B
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        C
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        D
+    </label>
+ </div>
+ <div class="question">
+                <p> 4. True or false:<br>
+
+                Is the code correct?<br>
+                
+                <img src="4.png" alt="four">
+
+<div class="answers">
+    <label>
+        <input type="radio" name="q1" value="a">
+        TRUE
+    </label>
+    <label>
+        <input type="radio" name="q1" value="a">
+        FALSE
+    </label>
+ </div>
+ 
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
